28b3c345e8fe5f63ed2f7881ec0bf2cec2f4946a
ShriramShanbhag <shriramshanbhag@hotmail.com>
2026-01-31 16:19:45 +0530

    configuired auth interceptor in provideHttpClient

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,15 +2,16 @@ import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessC
 import { provideRouter } from '@angular/router';
 import { provideHttpClient } from '@angular/common/http';
 import { provideAnimations } from '@angular/platform-browser/animations';
+import { withInterceptors } from '@angular/common/http';
 
 import { routes } from './app.routes';
-
+import { authInterceptor } from './core/auth.interceptor';
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
     provideRouter(routes),
-    provideHttpClient(),
-    provideAnimations()
+    provideHttpClient(withInterceptors([authInterceptor])),
+    provideAnimations(),
   ]
 };
