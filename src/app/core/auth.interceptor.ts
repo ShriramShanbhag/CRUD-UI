4f6b116d2170f67a2daecd716799b794e48f358e
ShriramShanbhag <shriramshanbhag@hotmail.com>
2026-01-31 16:18:47 +0530

    Added auth interceptor

---
+++ src/app/core/auth.interceptor.ts
@@ -0,0 +1,13 @@
+import { inject } from '@angular/core';
+import { HttpRequest, HttpHandlerFn} from '@angular/common/http';
+import { AuthService } from './auth.service';
+
+export function authInterceptor(req: HttpRequest<unknown>, next: HttpHandlerFn) {
+  // Inject the current `AuthService` and use it to get an authentication token:
+  const authToken = inject(AuthService).token();
+  // Clone the request to add the authentication header.
+  const newReq = req.clone({
+    headers: req.headers.append('Authorization', `Bearer ${authToken}`),
+  });
+  return next(newReq);
+}
